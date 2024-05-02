4c887db37c35153680c1e0492ee0b7d3a1b8971d
scottyp7 <scottparker960@gmail.com>
2024-05-02 15:46:49 +1000

    update

---
+++ Exercise 4/calculations.js
@@ -0,0 +1,59 @@
+function multiply(a, b)//Declares a function
+{ 
+
+console.log(a * b) //part of the function to tell it what calculation to do
+}
+
+function add(a, b)//Declares a function
+{ 
+return a + b
+}
+   
+function minus(a, b)//Declares a function
+{ 
+
+console.log(a - b) //part of the function to tell it what calculation to do
+}
+
+function divide(a, b)//Declares a function
+{ 
+
+console.log(a / b) //part of the function to tell it what calculation to do
+}
+console.log(add(7,9)) 
+console.log(minus(54,12))
+
+// if statements checks conditions between 2 elements and displays either true or false
+
+if(add(6,2)!=6){console.log("success")} // Completes the calculations and displys success if correct
+    else {console.log("failed")}; // displays failed is calc is not true
+
+if(minus(6,1)!=5){console.log("correct")}
+    else{console.log}("incorrect")
+
+if(divide(6,1)!=5){console.log("complete")}
+    else{console.log}("not complete")
+
+
+function PrintLinesOnConsole(Text)
+{
+    var name = 'Hello '
+    name += Text
+console.log(name)
+}
+
+
+
+PrintLinesOnConsole("Scott")
+
+function PrintLinesOnConsole2(Text2) // to print text onto the console
+{
+  let girlfriend = 'Sarah '
+  girlfriend += Text2 // defines girlfriend as the same as text 2
+console.log(girlfriend) // prints the value of girlfriend on the console
+}
+
+PrintLinesOnConsole2("Galloway") // prints girlfriend and text 2 on the console
+// the end result will be sarah galloway
+
+
